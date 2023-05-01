71d2260dbbfc2a3df17d2a7f12b7795a9cb6e95a
perfectrider <konsgrebenev@gmail.com>
2023-05-01 14:04:14 +0300

    db credentials, dockerfile, requirements added

--- products_service/settings.py
+++ products_service/settings.py
@@ -63,11 +63,11 @@ WSGI_APPLICATION = 'products_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
