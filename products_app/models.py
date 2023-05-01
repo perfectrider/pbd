4406a839b44de1a0de10d43729802f75f33c1302
perfectrider <konsgrebenev@gmail.com>
2023-05-01 11:18:21 +0300

    models created

--- products_app/models.py
+++ products_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Assortment(models.Model):
+    name = models.CharField(max_length=255, editable=False)
+    measure_unit = models.CharField(max_length=5, editable=False)
+
+
+class Division(models.Model):
+    name = models.CharField(max_length=255, editable=False)
